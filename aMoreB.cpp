2becb72ec8c23e912a8d13a2b1557c5d5bbc8193
Max <msxkurko3@gmail.com>
2021-12-03 15:25:58 +0200

    1

---
+++ aMoreB.cpp
@@ -0,0 +1,35 @@
+﻿using namespace std;
+#include <iostream>
+#include <string>
+
+int main()
+{
+    string userString;
+    int letterA = 0;
+    int letterB = 0;
+
+    cout << "Enter string :" << endl;
+    cin >> userString;
+
+    for (int i = 0; i < userString.size(); ++i)
+    {
+        if (userString[i] == 'a')
+        {
+            ++letterA;
+        }
+        if (userString[i] == 'b')
+        {
+            ++letterB;
+        }
+    }
+
+    if (letterA > letterB)
+    {
+        cout << "true" << endl;
+    }
+    else
+    {
+        cout << "false" << endl;
+    }
+}
+
